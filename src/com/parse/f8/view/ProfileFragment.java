e91bc6812b45a8f9166328103d87d070e424bb7b
Masood <masoodmx@gmail.com>
2015-02-24 00:27:35 +0100

    Update fragment. time picker almost done!

--- src/com/parse/f8/view/ProfileFragment.java
+++ src/com/parse/f8/view/ProfileFragment.java
@@ -10,10 +10,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -28,9 +32,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -53,38 +59,17 @@ public class ProfileFragment extends Fragment {
 	private EditText text_status;
 	private EditText text_location;
 	private ImageView imageMapPin;
+	private View datePicker;
+	private ImageView timePicker;
 	private String latitude = null;
 	private String longitude = null;
+	private int selectedHour;
+	private int selectedMin;
+	private int selectedYear;
+	private int selectedMonth;
+	private int selectedDay;
 	
 	public ProfileFragment() {
-		// Required empty public constructor
-		
-				
-		// Test Section //
-		
-		
-//		ParseObject newObj = new ParseObject("NewObj");
-//		newObj.put("ProjName", "Thesis");
-//		newObj.put("Author", "Masood Azizi");
-//		newObj.put("Deadline", "May 2015");
-////		newObj.saveInBackground();
-//
-//		
-//		ParseQuery<ParseObject> query = ParseQuery.getQuery("NewObj");
-//		query.whereEqualTo("ProjName", "Thesis");
-//		query.findInBackground(new FindCallback<ParseObject>() {
-//						
-//			@Override
-//			public void done(List<ParseObject> result, ParseException e) {
-//				if (e == null)
-//					Log.d("mylog", "Result is " + result.size());
-//				else
-//					Log.d("mylog", "Error! " + e.getMessage());
-//			}
-//		});
-		
-		
-		/////////////////
 		
 	}
 	
@@ -121,6 +106,26 @@ public class ProfileFragment extends Fragment {
 			    transaction.addToBackStack(null);
 			    transaction.setTransition(4099);	
 			    transaction.commit(); 
+			}
+		});
+		
+		datePicker = profileView.findViewById(R.id.view_date_picker);
+		datePicker.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				onDatePickerClicked();
+			}
+		});
+		
+		timePicker = (ImageView) profileView.findViewById(R.id.image_time_picker);
+		timePicker.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				onTimePickerClicked();
 			}
 		});
 		
@@ -167,31 +172,70 @@ public class ProfileFragment extends Fragment {
 	
 	private void onPostButtonClicked(View v) {
 
+		// Initialize
+		ParseObject postObj = new ParseObject("Post");
+		String name = fetchUserInfo("name");
+		postObj.put("owner", name);
 		
+		String fbId = fetchUserInfo("fbId");
+		postObj.put("userId", fbId);
+		
+		// STATUS update
 		text_status = (EditText) getActivity().findViewById(R.id.txt_status_text);
 		String status = text_status.getText().toString();
 		text_status.setText("");
+		postObj.put("text", status);
 		
+		// TIME update
 		///// !!! Timezone Problem !!! /////
 		text_current_time = (EditText) getActivity().findViewById(R.id.txt_time);
 		String time0Str = text_current_time.getText().toString();
-		SimpleDateFormat df = new SimpleDateFormat("MMM dd, yyyy, HH:mm");
+		SimpleDateFormat df0 = new SimpleDateFormat("MMM dd, yyyy, HH:mm");
 		TimeZone timeZone = TimeZone.getTimeZone("CET"); 
-		df.setTimeZone(timeZone);
+		df0.setTimeZone(timeZone);
 		Date time0 = new Date();
 		try {
-			time0 = df.parse(time0Str);
+			time0 = df0.parse(time0Str);
 		} catch (java.text.ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		String name = fetchUserInfo("name");
-		
-		ParseObject postObj = new ParseObject("Post");
-		postObj.put("owner", name);
-		postObj.put("text", status);
 		postObj.put("time0", time0);
+		
+		SimpleDateFormat df1 = new SimpleDateFormat("MMM dd, yyyy");
+		df1.setTimeZone(timeZone);
+		Date time1 = new Date();
+		try {
+			time1 = df1.parse(time0Str);
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		Calendar cal = Calendar.getInstance();
+//		Date dateNow = null;
+//		try {
+//			dateNow = new SimpleDateFormat("MMM dd, yyyy").parse(time0Str);
+//		} catch (java.text.ParseException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+		//cal.setTime(time0);
+		Log.d("TimeDebug", "" + cal.get(Calendar.YEAR) + cal.get(Calendar.MONTH) + 
+				cal.get(Calendar.DAY_OF_MONTH) + cal.get(Calendar.DAY_OF_WEEK));
+		
+		DateFormat dfHour = new SimpleDateFormat("HH");
+		dfHour.setTimeZone(timeZone);
+		Date hour = new Date();
+		try {
+			hour = dfHour.parse(time0Str);
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.d("TimeDebug", hour.toString());
+		
+		
 		postObj.saveInBackground();
 	}
 
@@ -241,7 +285,7 @@ public class ProfileFragment extends Fragment {
 	private String fetchUserInfo(String type) {
 		
 		SharedPreferences userInfoPref = this.getActivity().getSharedPreferences(USER_INFO_PREFS, 0);
-		String userInfo = userInfoPref.getString(type, "None");
+		String userInfo = userInfoPref.getString(type, null);
 		
 		return userInfo;
 	}
@@ -274,5 +318,48 @@ public class ProfileFragment extends Fragment {
 	    editor.remove("latitude");
 	    editor.remove("longitude");
 		editor.commit();
+	}
+	
+	private void onDatePickerClicked() {
+		
+		final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+		DatePickerDialog dpd = new DatePickerDialog(getActivity(), 
+				new OnDateSetListener() {
+					
+					@Override
+					public void onDateSet(DatePicker view, int year, int monthOfYear,
+							int dayOfMonth) {
+						
+						selectedYear = year;
+						selectedMonth = monthOfYear;
+						selectedDay = dayOfMonth;	
+					}
+				}, mYear, mMonth, mDay);
+		dpd.show();
+	}
+	
+	private void onTimePickerClicked() {
+		
+		// Process to get Current Time
+        final Calendar c = Calendar.getInstance();
+        int mHour = c.get(Calendar.HOUR_OF_DAY);
+        int mMinute = c.get(Calendar.MINUTE);
+        
+        // Launch Time Picker Dialog
+        TimePickerDialog tpd = new TimePickerDialog(getActivity(),
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                            int minute) {
+                    
+                    	selectedHour = hourOfDay;
+                    	selectedMin = minute;
+                        	                    }
+                }, mHour, mMinute, false);
+        tpd.show();
 	}
 }
